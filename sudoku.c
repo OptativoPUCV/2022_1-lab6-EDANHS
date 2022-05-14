00b9c59d680d5c7903b0e4cf9124285c6b8a5838
Your Name <you@example.com>
2022-05-14 18:15:41 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -128,7 +128,21 @@ int is_final(Node* n){
 }
 
 Node* DFS(Node* initial, int* cont){
+  Stack *pila = createStack();
+  push(pila,initial);
+  while(pila != NULL){
+    Node *n = top(pila);
+    pop(pila);
+    if(is_final(n) == 1) return n;
 
+    //visita
+    List *adj = get_adj_nodes(n);
+    Node *aux = first(adj);
+    while(aux != NULL){
+      push(pila,aux);
+      aux = next(adj);
+    }
+  }
   return NULL;
 }
 
