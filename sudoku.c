3a228416ab22b88ef4cf88366e6567f86de33162
Your Name <you@example.com>
2022-05-14 16:45:40 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -78,11 +78,6 @@ int is_valid(Node* n){
   }
 
   //ileras
-  for(i = 0 ; i < 9 ; i++){
-    for(j = 0 ; j < 9 ;j++){
-      if(recorrer_ileras(n,i,j) == 1) return 0;
-    }
-  }
   
 
     return 1;
